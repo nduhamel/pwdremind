c016457dd0e9c4834f5cf5e363f391a727d44934
Nicolas Duhamel <nicolas@jombi.fr>
2012-02-03 19:27:37 +0100

    first commit

---
+++ static/js/valid.js
@@ -0,0 +1,175 @@
+(function($) {
+    /*
+    Validation Singleton
+    */
+    var Validation = function() {
+
+        var rules = {
+
+            email : {
+               check: function(value) {
+
+                   if(value)
+                       return testPattern(value,"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])");
+                   return true;
+               },
+            },
+            url : {
+
+               check : function(value) {
+
+                   if(value)
+                       return testPattern(value,"^https?://(.+\.)+.{2,4}(/.*)?$");
+                   return true;
+               },
+            },
+            required : {
+
+               check: function(value) {
+
+                   if(value)
+                       return true;
+                   else
+                       return false;
+               },
+            }
+        }
+        var testPattern = function(value, pattern) {
+
+            var regExp = new RegExp(pattern,"");
+            return regExp.test(value);
+        }
+        return {
+
+            addRule : function(name, rule) {
+
+                rules[name] = rule;
+            },
+            getRule : function(name) {
+
+                return rules[name];
+            }
+        }
+    }
+
+    /*
+    Form factory
+    */
+    var Form = function(form) {
+
+        var fields = [];
+
+        form.find("[validation]").each(function() {
+            var field = $(this);
+            if(field.attr('validation') !== undefined) {
+                fields.push(new Field(field));
+            }
+        });
+        this.fields = fields;
+    }
+    Form.prototype = {
+        validate : function() {
+
+            for(field in this.fields) {
+
+                this.fields[field].validate();
+            }
+        },
+        isValid : function() {
+
+            for(field in this.fields) {
+
+                if(!this.fields[field].valid) {
+
+                    this.fields[field].field.focus();
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /*
+    Field factory
+    */
+    var Field = function(field) {
+
+        this.field = field;
+        this.valid = false;
+        this.attach("change");
+    }
+    Field.prototype = {
+
+        attach : function(event) {
+
+            var obj = this;
+            if(event == "change") {
+                obj.field.bind("change",function() {
+                    return obj.validate();
+                });
+            }
+            if(event == "keyup") {
+                obj.field.bind("keyup",function(e) {
+                    return obj.validate();
+                });
+            }
+        },
+        validate : function() {
+
+            var obj = this,
+                field = obj.field,
+                errorClass = "error",
+                types = field.attr("validation").split(" "),
+                container = field.closest('.clearfix'),
+                errors = [];
+
+            field.next(".error").remove();
+            for (var type in types) {
+
+                var rule = $.Validation.getRule(types[type]);
+                if(!rule.check(field.val())) {
+
+                    container.addClass("error");
+                    errors.push("");
+                }
+            }
+            if(errors.length) {
+
+                obj.field.unbind("keyup")
+                obj.attach("keyup");
+                obj.valid = false;
+            }
+            else {
+                container.removeClass("error");
+                obj.valid = true;
+            }
+        }
+    }
+
+    /*
+    Validation extends jQuery prototype
+    */
+    $.extend($.fn, {
+
+        validation : function() {
+
+            var validator = new Form($(this));
+            $.data($(this)[0], 'validator', validator);
+
+            $(this).bind("submit", function(e) {
+                validator.validate();
+                if(!validator.isValid()) {
+                    e.preventDefault();
+                }
+            });
+        },
+        validate : function() {
+
+            var validator = $.data($(this)[0], 'validator');
+            validator.validate();
+            return validator.isValid();
+
+        }
+    });
+    $.Validation = new Validation();
+})(jQuery);
---
+++ sync.php
@@ -0,0 +1,239 @@
+<?php
+
+$app = new PwdRemind();
+$app->route();
+
+
+class PwdRemind{
+
+    private $db;
+    private $session;
+
+    public function __construct(){
+        $this->db = new Database('test.db');
+        $this->session = new Session();
+    }
+
+    public function route(){
+        if (isset($_GET['action'])){
+            $action = $_GET['action'];
+            switch( $action ) {
+                case 'isloggedin':
+                    echo $this->session->is_logged_in();
+                    exit;
+                case 'logout':
+                    $this->session->logout();
+                    echo 1;
+                    exit;
+            }
+        }
+
+        if (isset($_POST['username']) && isset($_POST['password'])){
+            $this->try_login();
+        }
+
+        if (isset($_POST['index'])){
+            if($this->session->is_logged_in()){
+                echo sync($this->db, $this->session->get_username() );
+            }else{
+                error_die('Not logged');
+            }
+        }
+    }
+
+    public function try_login(){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        unset($_POST['username']);
+        unset($_POST['password']);
+        $local_hash = $this->db->get_user_pwd_hash($username);
+        if($local_hash == hash('sha256', $password)){
+            $this->session->login($username);
+            echo json_encode(array('username' => $username));
+            exit;
+        }else{
+            error_die('Invalid login params');
+        }
+    }
+
+}
+
+function error_die($message){
+    echo json_encode(array('error' => $message));
+    exit;
+}
+
+function sync($db, $username){
+    // Parse incoming data
+    $remote_index = (array)json_decode(stripslashes($_POST['index']), true);
+    unset($_POST['index']);
+    $remote_entries = $_POST;
+
+    // Get local index
+    $local_index = $db->get_local_index($username);
+    $diff_index = array_diff_key($local_index, $remote_index);
+
+    foreach ($remote_index as $id => $item) {
+        if (array_key_exists($id, $local_index)) {
+            if ($item == 0) {
+                // Remote entry has been deleted, remove the local too
+                $local_index[$id] = $item;
+                $db->delete_entry($id,$username);
+                unset($remote_index[$id]);
+                unset($remote_entries[$id]);
+            } elseif ($local_index[$id] == 0) {
+                // Local entry has been deleted, remove local entry too
+                $remote_index[$id] = $local_index[$id];
+                $db->delete_entry($id, $username);
+                unset($remote_index[$id]);
+                unset($remote_entries[$id]);
+            } elseif ($item['timestamp'] == $local_index[$id]['timestamp']) {
+                // Local entry is already the latest, don't send it back
+                unset($remote_entries[$id]);
+            }
+        } else {
+            $local_index[$id] = $remote_index[$id];
+            $db->store_entry($id, stripslashes($_POST[$id]),$username);
+            unset($remote_entries[$id]);
+        }
+    }
+
+    foreach ($diff_index as $id => $data) {
+        if ($local_index[$id] !== 0) {
+            $remote_entries[$id] = $db->get_entry($id,$username);
+            $remote_index[$id] = $local_index[$id];
+        } else {
+            unset($remote_entries[$id]);
+            unset($remote_index[$id]);
+        }
+    }
+
+    $return = array('index' => $remote_index, 'entries' => $remote_entries);
+    return json_encode($return);
+}
+
+class Session {
+
+    public function __construct() {
+        session_start();
+    }
+
+    public function login($userid){
+        session_regenerate_id ();
+        $_SESSION['valid'] = True;
+        $_SESSION['username'] = $userid;
+    }
+
+    public function is_logged_in(){
+        if(isset($_SESSION['valid']) && $_SESSION['valid'])
+            return true;
+        return false;
+    }
+
+    public function logout(){
+        $_SESSION = array(); //destroy all of the session variables
+        session_destroy();
+    }
+
+    public function get_username(){
+        return $_SESSION['username'];
+    }
+}
+
+
+class Database {
+    private $db;
+
+    public function __construct($dbname) {
+        $this->db = sqlite_open($dbname, 0666, $error);
+        if (!$this->db) die ($error);
+        $this->check_table();
+    }
+
+    public function __destruct() {
+        sqlite_close($this->db);
+    }
+
+    public function sqlite_quote_string($str) {
+        if (get_magic_quotes_gpc()) {
+            $str = stripslashes($str);
+        }
+        return sqlite_escape_string($str);
+    }
+
+    public function db_query($stm){
+        $result = sqlite_query($this->db, $stm, $error);
+        if (!$result) die("Cannot execute query. $error");
+        return $result;
+    }
+
+    public function get_user_pwd_hash($username){
+        $username = $this->sqlite_quote_string($username);
+        $result = $this->db_query("SELECT password FROM users WHERE username = '$username'");
+        if(sqlite_num_rows($result)>0){
+              return sqlite_fetch_single($result);
+        }
+        return False;
+    }
+
+    public function get_local_index($username){
+        $username = $this->sqlite_quote_string($username);
+        $result = $this->db_query("SELECT id FROM password WHERE username='$username'");
+        $local_index = array();
+        if(sqlite_num_rows($result)>0){
+            while(sqlite_has_more($result)){
+              $row=sqlite_fetch_single($result);
+              $local_index[$row] = $row;
+            }
+        }
+        return $local_index;
+    }
+
+    public function get_entry($id, $username) {
+        $id = $this->sqlite_quote_string($id);
+        $username = $this->sqlite_quote_string($username);
+        $result = $this->db_query("SELECT data FROM password WHERE id='$id' AND username='$username'");
+        if (sqlite_num_rows($result)>0) {
+            $entry =  json_decode(sqlite_fetch_single($result), true);
+            if($entry){ return $entry; }
+        }
+        return array();
+    }
+
+    public function store_entry($id, $data, $username) {
+        $id = $this->sqlite_quote_string($id);
+        $data = $this->sqlite_quote_string($data);
+        $username = $this->sqlite_quote_string($username);
+        $result = $this->db_query("INSERT INTO password VALUES( '$id' , '$data', '$username')");
+    }
+
+    public function delete_entry($id, $username) {
+        $id = $this->sqlite_quote_string($id);
+        $username = $this->sqlite_quote_string($username);
+        $result = $this->db_query("DELETE FROM password WHERE id='$id' AND username='$username'");
+    }
+
+    private function check_table(){
+        $stm = "SELECT * FROM password";
+        $q = sqlite_exec($this->db, $stm, $error);
+        if (!$q) {
+            $stm = "CREATE TABLE password (id TEXT, data TEXT, username TEXT)";
+            $ok = sqlite_exec($this->db, $stm, $error);
+            if (!$ok)
+                die("Cannot execute query. $error");
+        }
+        $stm = "SELECT * FROM users";
+        $q = sqlite_exec($this->db, $stm, $error);
+        if (!$q) {
+            $stm = "CREATE TABLE users (id INTEGER PRIMARY KEY, username VARCHAR(30) NOT NULL UNIQUE, password VARCHAR(64) NOT NULL);";
+            $ok = sqlite_exec($this->db, $stm, $error);
+            if (!$ok)
+                die("Cannot execute query. $error");
+            $hash = hash('sha256', 'toto');
+            $stm = "INSERT INTO users ( username, password ) VALUES ( 'nicolas' , '$hash')";
+            $ok = sqlite_exec($this->db, $stm, $error);
+            if (!$ok)
+                die("Cannot execute query. $error");
+        }
+    }
+}
