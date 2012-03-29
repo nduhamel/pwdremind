c3fe7e3911484b22e150099431a7cbe0511d487c
Nicolas Duhamel <nicolas@jombi.fr>
2012-03-29 18:11:35 +0200

    Remove unused timeout plugin code

--- www/static/js/pwdremindui.js
+++ www/static/js/pwdremindui.js
@@ -55,39 +55,3 @@
     return methods.init.call(this,pwdremind);
   };
 })( jQuery );
-
-
-//
-// Keepalive timeout
-//
-var timeoutHandler = function(){
-    if (!$.data( $('#keepalive-modal')[0], 'idletimeout')){
-        console.log('Start time out');
-        $.idleTimeout('#keepalive-modal', '#keepalive-modal #btn-keepalive', {
-            idleAfter: 120, // user is considered idle after 2 minutes of no movement
-            pollingInterval: 60, // a request to keepalive.php (below) will be sent to the server every minute
-            keepAliveURL: 'sync.php?action=keepalive',
-            serverResponseEquals: 'OK', // the response from keepalive.php must equal the text "OK"
-            onTimeout: function(){
-                console.log('timeout');
-                document.pwdremind.logout();
-                $('#keepalive-modal').modal('hide');
-            },
-            onIdle: function(){
-                    $('#keepalive-modal').modal({
-                        show: true,
-                        backdrop: 'static',
-                    });
-            },
-            onCountdown: function(counter){
-                    // update the counter span inside the dialog during each second of the countdown
-            },
-            onResume: function(){
-                $('#keepalive-modal').modal('hide');
-            }
-        });
-    }else{
-        $(document).trigger("idletimeout/restart");
-    }
-};
-
