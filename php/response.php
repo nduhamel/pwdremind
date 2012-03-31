90316b2618ea8add42791abfce2a90ab2451d9e8
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-03-31 12:03:49 +0200

    Fix exception error in response class

--- php/response.php
+++ php/response.php
@@ -1,48 +1,53 @@
 <?php
+
 require_once('hermetic/crypto_util.php');
 
 class Response {
 
-	private $status;
-	private $msg;
-	private $data;
-	private $macKey;
-	private $sig;
-
+	private $_status;
+	private $_msg;
+	private $_data;
+	private $_macKey;
+	private $_sig;
 
 	public function __construct($key=NULL){
 		if (!is_null($key)){
 			$this->setKey($key);
 		}else{
-			$this->macKey = NULL;
+			$this->_macKey = NULL;
 		}
 	}
 
 	private function sign($data){
-		if ( is_null($this->macKey) ){
-			throw new Exception("INTERNAL_ERROR");
+		if ( is_null($this->_macKey) ){
+			throw new Exception("EMPTY_MACKEY");
 		}
-		return hmac($this->macKey, $data);
+		return hmac($this->_macKey, $data);
 	}
 
 	public function setKey($key){
-		$this->macKey = substr($key,32,40);
+		$this->_macKey = substr($key,32,40);
 	}
 
 	public function error($reason){
-		$this->status = "ERROR";
-		$this->msg = $reason;
+		$this->_status = "ERROR";
+		$this->_msg = $reason;
 	}
 
 	public function data($data){
-		$this->status = "OK";
-		$this->data = json_encode($data);
-		$this->sig = $this->sign($this->data);
+		$this->_status = "OK";
+		$this->_data = json_encode($data);
+		try { 
+			$this->_sig = $this->sign($this->_data); 
+		}
+		catch (Exception $e){
+			echo 'ERROR : ', $e->getMessage();
+		}
 	}
 
 	public function message($msg){
-		$this->status = "OK";
-		$this->msg = $msg;
+		$this->_status = "OK";
+		$this->_msg = $msg;
 	}
 
 	public function send(){
@@ -52,10 +57,10 @@ class Response {
 	
 	public function check_error(){
 		
-		if ( is_null($this->status) ){
+		if ( is_null($this->_status) ){
 			$this->error("INVALID_REQUEST");
 		} 
-		elseif ( is_null($this->data) && is_null($this->msg) ){
+		elseif ( is_null($this->_data) && is_null($this->_msg) ){
 			$this->error("INTERNAL_ERROR");
 		}
 		
@@ -64,10 +69,10 @@ class Response {
 	public function __toString(){
 		
 		$data = array(
-			'status' => $this->status,
-			'data' => $this->data,
-			'msg' => $this->msg,
-			'sig' => $this->sig
+			'status' => $this->_status,
+			'data' => $this->_data,
+			'msg' => $this->_msg,
+			'sig' => $this->_sig
 		);
 		
 		return json_encode($data);
