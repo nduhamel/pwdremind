5a7b81e1bf8f992b91dda313122003f381b77baa
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-03-31 18:58:05 +0200

    Created Authentication class

--- www/auth.php
+++ www/auth.php
@@ -7,46 +7,81 @@ ini_set("log_errors" , "1");
 ini_set("error_log" , "Errors.log.txt");
 ini_set("display_errors" , "0");
 
-$db = new Database('../test.db');
-
-$result = NULL;
-
-$username = '';
-$A = '';
-$M1 = '';
-$validRequest = false;
-
-if ( isset($_POST['username']) && isset($_POST['A']) ) {
+//Todo
+if (isset($_POST['username']))
 	$username = $_POST['username'];
+else
+	$username = NULL;
+
+if (isset($_POST['A']))
 	$A = $_POST['A'];
-	if ( strlen($A)==strlen($srpOptions->Nhex()) && isHex($A)) {
-		$validRequest = true;
-		if ( isset($_POST['M1'])) {
-			$M1 = $_POST['M1'];
-			if (strlen($M1)==40 && isHex($M1)) { // 40hex digits = 160bits
-				$validRequest = true;
-			}else{
-				$validRequest = false;
+else
+	$A = NULL;
+
+if (isset($_POST['M1']))
+	$M1 = $_POST['M1'];
+else
+	$M1 = NULL;
+//----
+
+$auth = new Authentication(new Database('../test.db'),$username,$A,$M1);
+$auth->run();
+
+class Authentication {
+	
+	private $_result;
+	private $_username;
+	private $_A;
+	private $_M1;
+	private $_db;
+	private $_srpOptions;
+	
+	public function __construct(Database $db,$username,$A,$M1){
+		$this->_username = $username;
+		$this->_A = $A;
+		$this->_M1 = $M1;
+		$this->_db = $db;
+		$this->_result = NULL;
+		$this->_srpOptions = new SRP_SHA1_256();
+	}
+	
+	private function _is_validRequest(){
+		$isValid = false;
+		
+		if ( isset($this->_username) && isset($this->_A) ) {
+			if ( strlen($this->_A)==strlen($this->_srpOptions->Nhex()) && isHex($this->_A)) {
+				$isValid = true;
+				if ( isset($this->_M1)) {
+					if (strlen($this->_M1)==40 && isHex($this->_M1)) { // 40hex digits = 160bits
+						$isValid = true;
+					}else{
+						$isValid = false;
+					}
+				}
 			}
 		}
-
+		return $isValid;
 	}
-}
-
-if ( $validRequest ) {
-	if (empty($M1)) { // public key exchange
-		$user_data = $db->get_verifier($username);
-		if ($user_data!=NULL && $user_data->verifier!=NULL) {
-			$result = publicKeyExchange($username,$user_data->salt, $user_data->verifier, $A );
+	
+	public function run(){
+		
+		if ( $this->_is_validRequest() ) {
+			if (empty($this->_M1)) { // public key exchange
+				$user_data = $this->_db->get_verifier($this->_username);
+				if (!is_null($user_data) && !is_null($user_data->verifier)) {
+					$result = publicKeyExchange($this->_username,$user_data->salt, $user_data->verifier, $this->_A );
+				} else {
+					$result->status = "ERROR";
+					$result->msg = "USER_DOES_NOT_EXIST";
+				}
+			} else { // shared key verification
+				$result = sharedKeyVerification($this->_username, $this->_A, $this->_M1);
+			}
 		} else {
 			$result->status = "ERROR";
-			$result->reason = "USER_DOES_NOT_EXIST";
+			$result->msg = "INVALID_REQUEST";
 		}
-	} else { // shared key verification
-		$result = sharedKeyVerification($username, $A, $M1);
+		echo json_encode($result);
+		
 	}
-} else {
-	$result->status = "ERROR";
-	$result->reason = "INVALID_REQUEST";
 }
-echo json_encode($result);
