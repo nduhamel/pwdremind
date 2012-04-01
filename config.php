9d1afe960bf9e500ca8ba1ac4073df0db28aa403
Nicolas Duhamel <nicolas@jombi.fr>
2012-04-01 15:46:50 +0200

    Add postgresql compatibility

--- config.php
+++ config.php
@@ -4,4 +4,8 @@
 */
 
 // Data Source Name for database connection see: http://www.php.net/manual/en/pdo.drivers.php
+// sqlite exemple:
+//  sqlite:../test.db
+// postgresql exemple:
+//  pgsql:host=localhost;port=5432;dbname=pwdremind;user=pwdremind;password=pwdremind
 define('PDO_DNS', 'sqlite:../test.db');
--- script/install_db.php
+++ script/install_db.php
@@ -2,7 +2,18 @@
 require_once('../config.php');
 
 $db = new PDO(PDO_DNS);
-$stm = "CREATE TABLE password (id INTEGER PRIMARY KEY, data TEXT, username TEXT)";
-$db->query($stm);
-$stm = "CREATE TABLE users (id INTEGER PRIMARY KEY, username VARCHAR(128) NOT NULL UNIQUE, verifier VARCHAR(256) NOT NULL, salt VARCHAR(32) NOT NULL );";
-$db->query($stm);
+
+$driver = $db->getAttribute(PDO::ATTR_DRIVER_NAME);
+
+if ($driver == 'pgsql'){
+    $stm = "CREATE TABLE password (id SERIAL PRIMARY KEY, data TEXT, username TEXT)";
+    $db->query($stm);
+    $stm = "CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(128) NOT NULL UNIQUE, verifier VARCHAR(256) NOT NULL, salt VARCHAR(32) NOT NULL );";
+    $db->query($stm);
+}else{
+    $stm = "CREATE TABLE password (id INTEGER PRIMARY KEY, data TEXT, username TEXT)";
+    $db->query($stm);
+    $stm = "CREATE TABLE users (id INTEGER PRIMARY KEY, username VARCHAR(128) NOT NULL UNIQUE, verifier VARCHAR(256) NOT NULL, salt VARCHAR(32) NOT NULL );";
+    $db->query($stm);
+}
+
