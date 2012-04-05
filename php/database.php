796ee688a13f68825aa62f487ceb6b330f678215
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-04-05 11:54:08 +0200

    db clean up

--- php/database.php
+++ php/database.php
@@ -20,11 +20,11 @@ class Database {
         $this->_db = NULL;
     }
 
-    public function get_verifier($username){
-        $req = $this->_db->prepare("SELECT * FROM users WHERE username = :username");
-        $req->execute(array('username'=>$username));
-        return $req->fetchObject();
-    }
+        public function get_verifier($username){
+                $req = $this->_db->prepare("SELECT * FROM user WHERE username = :username");
+                $req->execute(array('username'=>$username));
+                return $req->fetchObject();
+        }
 
     public function get_entries($username){
         $req = $this->_db->prepare("SELECT id,data FROM password WHERE username = :username");
@@ -66,18 +66,26 @@ class Database {
             return false;
     }
 
-    private function createTables(){
-        $stm = "CREATE TABLE password (id INTEGER PRIMARY KEY, data TEXT, username TEXT)";
-        $this->_db->query($stm);
-        $stm = "CREATE TABLE users (id INTEGER PRIMARY KEY,
-                username VARCHAR(128) NOT NULL UNIQUE, verifier
-                VARCHAR(256) NOT NULL, salt VARCHAR(32) NOT NULL );";
-        $this->_db->query($stm);
+        public function createTables() {
 
-        // admin:admin
-        $stm = "INSERT INTO users (username, verifier, salt)
-        VALUES ('admin', '01073e301b235e616c19eddd48cadf1c48c0ed83c1e5a2cb0841ab892808b4468e',
-        'ee8528c459ba7fcfb5a6');";
-        $this->_db->query($stm);
-    }
+                $req = "CREATE TABLE password (
+                                id INTEGER PRIMARY KEY,
+                                data TEXT,
+                                username VARCHAR(128) NOT NULL)";
+                $this->_db->query($req);
+
+                $req = "CREATE TABLE user (
+                                id INTEGER PRIMARY KEY,
+                                username VARCHAR(128) NOT NULL UNIQUE,
+                                verifier VARCHAR(256) NOT NULL,
+                                salt VARCHAR(32) NOT NULL );";
+                $this->_db->query($req);
+
+                // admin:admin
+                $req = "INSERT INTO user (username, verifier, salt)
+                                VALUES ('admin',
+                                                '01073e301b235e616c19eddd48cadf1c48c0ed83c1e5a2cb0841ab892808b4468e',
+                                                'ee8528c459ba7fcfb5a6');";
+                $this->_db->query($req);
+        }
 }
