fad842159ec17f125562b04a8545ccbe7ca5f292
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-04-10 20:52:28 +0200

    Install script update

--- script/db/mysql_db.sql
+++ script/db/mysql_db.sql
@@ -1,42 +1,44 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
- 
+
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
 USE `mydb` ;
- 
+
 -- -----------------------------------------------------
 -- Table `mydb`.`user`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `mydb`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `username` VARCHAR(128) NOT NULL ,
+  `username` VARCHAR(256) NOT NULL ,
   `verifier` VARCHAR(256) NOT NULL ,
   `salt` VARCHAR(32) NOT NULL ,
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) ,
+  `category` TEXT NOT NULL ,
+  `config` TEXT NOT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB
 COMMENT = '\n';
- 
- 
+
+
 -- -----------------------------------------------------
--- Table `mydb`.`password`
+-- Table `mydb`.`data`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`password` (
+CREATE  TABLE IF NOT EXISTS `mydb`.`data` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `data` TEXT NULL ,
-  `username` VARCHAR(128) NOT NULL ,
+  `category` VARCHAR(256) NULL ,
+  `user_id` INT NOT NULL ,
   PRIMARY KEY (`id`) ,
-  INDEX `fk_password_users` (`username` ASC) ,
-  CONSTRAINT `fk_password_users`
-    FOREIGN KEY (`username` )
-    REFERENCES `mydb`.`user` (`username` )
+  INDEX `fk_data_user1` (`user_id` ASC) ,
+  CONSTRAINT `fk_data_user1`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `mydb`.`user` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
- 
- 
- 
+
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- script/install_db.php
+++ script/install_db.php
@@ -8,31 +8,37 @@ if ($driver == 'pgsql') {
 	
 	$req = "CREATE TABLE data (
 			id SERIAL PRIMARY KEY, 
-			data TEXT, 
-			username VARCHAR(128) NOT NULL)";
+			data TEXT,
+			category VARCHAR(256),
+			user_id INT NOT NULL)";
 	$db->query($req);
 	
 	$req = "CREATE TABLE user (
 			id SERIAL PRIMARY KEY, 
-			username VARCHAR(128) NOT NULL UNIQUE, 
-			verifier VARCHAR(256) NOT NULL, 
-			salt VARCHAR(32) NOT NULL );";
+			username VARCHAR(256) NOT NULL UNIQUE,
+			verifier VARCHAR(256) NOT NULL,
+			salt VARCHAR(32) NOT NULL,
+			category TEXT NOT NULL,
+			config TEXT NOT NULL);";
 	$db->query($req);
 }
 else {
 	
 	$req = "CREATE TABLE data (
 			id INTEGER PRIMARY KEY, 
-			data TEXT, 
-			username VARCHAR(128) NOT NULL)";
+			data TEXT,
+			category VARCHAR(256),
+			user_id INT NOT NULL)";
 	$db->query($req);
 	
 	$req = "CREATE TABLE user (
-			id INTEGER PRIMARY KEY, 
-			username VARCHAR(128) NOT NULL UNIQUE, 
-			verifier VARCHAR(256) NOT NULL, 
-			salt VARCHAR(32) NOT NULL );";
+			id INTEGER PRIMARY KEY,
+			username VARCHAR(256) NOT NULL UNIQUE,
+			verifier VARCHAR(256) NOT NULL,
+			salt VARCHAR(32) NOT NULL,
+			category TEXT NOT NULL,
+			config TEXT NOT NULL);";
 	$db->query($req);
-
+	
 }
 
