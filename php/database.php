8db5f27bc61c14278374326646df1bf7887d452e
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-04-10 18:57:20 +0200

    Improved DB model

--- php/database.php
+++ php/database.php
@@ -25,26 +25,26 @@ class Database {
 	}
 
 	public function get_entries($username){
-		$req = $this->_db->prepare("SELECT id,data FROM data WHERE username = :username");
-		$req->execute(array('username'=>$username));
+		$req = $this->_db->prepare("SELECT id,data FROM data WHERE user_id = :user_id");
+		$req->execute(array('user_id'=>$user_id));
 		return $req->fetchall(PDO::FETCH_ASSOC);
 	}
 
-	public function store_entry($data, $username) {
-		$req = $this->_db->prepare("INSERT INTO data (data, username)  VALUES(:data, :username)");
-		$req->execute(array('data'=> $data,'username'=>$username));
+	public function store_entry($data, $user_id) {
+		$req = $this->_db->prepare("INSERT INTO data (data, user_id)  VALUES(:data, :user_id)");
+		$req->execute(array('data'=> $data,'user_id'=>$user_id));
 		return $this->_db->lastInsertId();
 	}
 
-	public function delete_entry($id, $username) {
-		$req = $this->_db->prepare("DELETE FROM data WHERE id=:id AND username=:username ");
-		$req->execute(array('id'=>$id, 'username'=>$username));
+	public function delete_entry($entry_id, $user_id) {
+		$req = $this->_db->prepare("DELETE FROM data WHERE id=:entry_id AND user_id=:user_id ");
+		$req->execute(array('entry_id'=>$entry_id, 'user_id'=>$user_id));
 		return $id;
 	}
 	
-	public function check_entry($id, $username) {
-		$req = $this->_db->prepare("SELECT id FROM data WHERE id=:id AND username=:username ");
-		$req->execute(array('id'=>$id, 'username'=>$username));
+	public function check_entry($id, $user_id) {
+		$req = $this->_db->prepare("SELECT id FROM data WHERE id=:id AND user_id=:user_id ");
+		$req->execute(array('id'=>$id, 'user_id'=>$user_id));
 		$req = $req->fetchall(PDO::FETCH_ASSOC);
 		if (count($req) == 1)
 			return true;
@@ -52,26 +52,4 @@ class Database {
 			return false;
 	}
 
-	public function createTables() {
-		
-		$req = "CREATE TABLE data (
-				id INTEGER PRIMARY KEY, 
-				data TEXT, 
-				username VARCHAR(128) NOT NULL)";
-		$this->_db->query($req);
-		
-		$req = "CREATE TABLE user (
-				id INTEGER PRIMARY KEY, 
-				username VARCHAR(128) NOT NULL UNIQUE, 
-				verifier VARCHAR(256) NOT NULL, 
-				salt VARCHAR(32) NOT NULL );";
-		$this->_db->query($req);
-
-		// admin:admin
-		$req = "INSERT INTO user (username, verifier, salt) 
-				VALUES ('admin', 
-						'01073e301b235e616c19eddd48cadf1c48c0ed83c1e5a2cb0841ab892808b4468e', 
-						'ee8528c459ba7fcfb5a6');";
-		$this->_db->query($req);
-	}
 }
