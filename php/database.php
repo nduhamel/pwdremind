2383379017edc9ec3f2ea37ac94a8374ea13712a
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-04-11 15:03:57 +0200

    Indentation

--- php/database.php
+++ php/database.php
@@ -20,72 +20,72 @@ class Database {
         $this->_db = NULL;
     }
 
-        public function get_verifier($username){
-                $req = $this->_db->prepare("SELECT * FROM user WHERE username = :username");
-                $req->execute(array('username'=>$username));
-                return $req->fetchObject();
-        }
+    public function get_verifier($username){
+        $req = $this->_db->prepare("SELECT * FROM user WHERE username = :username");
+        $req->execute(array('username'=>$username));
+        return $req->fetchObject();
+    }
 
-        public function get_info($user_id){
-                $req = $this->_db->prepare("SELECT category, config FROM user WHERE id = :user_id");
-                $req->execute(array('user_id'=>$user_id));
-                return $req->fetchObject();
-        }
+    public function get_info($user_id){
+        $req = $this->_db->prepare("SELECT category, config FROM user WHERE id = :user_id");
+        $req->execute(array('user_id'=>$user_id));
+        return $req->fetchObject();
+    }
 
-        public function update_categories($uuid, $user_id) {
-            $req = $this->_db->prepare("UPDATE user SET category=:uuid WHERE id = :user_id ");
-            $req->execute(array('uuid'=>$uuid, 'user_id'=>$user_id));
-            return $cat;
-        }
+    public function update_categories($uuid, $user_id) {
+        $req = $this->_db->prepare("UPDATE user SET category=:uuid WHERE id = :user_id ");
+        $req->execute(array('uuid'=>$uuid, 'user_id'=>$user_id));
+        return $cat;
+    }
 
-        public function get_entries($user_id){
-                $req = $this->_db->prepare("SELECT id,data FROM data WHERE user_id = :user_id");
-                $req->execute(array('user_id'=>$user_id));
-                return $req->fetchall(PDO::FETCH_ASSOC);
-        }
+    public function get_entries($user_id){
+        $req = $this->_db->prepare("SELECT id,data FROM data WHERE user_id = :user_id");
+        $req->execute(array('user_id'=>$user_id));
+        return $req->fetchall(PDO::FETCH_ASSOC);
+    }
 
-        public function store_entry($data, $user_id) {
-            if ($this->_driver == 'pgsql'){
-                $req = $this->_db->prepare("INSERT INTO data (data, user_id)  VALUES(:data, :user_id) RETURNING id");
-                $req->execute(array('data'=> $data,'user_id'=>$user_id));
-                return $req->fetchColumn();
-            }else{
-                $req = $this->_db->prepare("INSERT INTO data (data, user_id)  VALUES(:data, :user_id)");
-                $req->execute(array('data'=> $data,'user_id'=>$user_id));
-                return $this->_db->lastInsertId();
-            }
+    public function store_entry($data, $user_id) {
+        if ($this->_driver == 'pgsql'){
+            $req = $this->_db->prepare("INSERT INTO data (data, user_id)  VALUES(:data, :user_id) RETURNING id");
+            $req->execute(array('data'=> $data,'user_id'=>$user_id));
+            return $req->fetchColumn();
+        }else{
+            $req = $this->_db->prepare("INSERT INTO data (data, user_id)  VALUES(:data, :user_id)");
+            $req->execute(array('data'=> $data,'user_id'=>$user_id));
+            return $this->_db->lastInsertId();
         }
+    }
 
-        public function update_entry($id,$data, $user_id) {
-            $req = $this->_db->prepare("UPDATE data SET data=:data WHERE id=:id AND user_id=:user_id ");
-            $req->execute(array('data'=>$data,'id'=>$id, 'user_id'=>$user_id));
-            return $id;
-        }
+    public function update_entry($id,$data, $user_id) {
+        $req = $this->_db->prepare("UPDATE data SET data=:data WHERE id=:id AND user_id=:user_id ");
+        $req->execute(array('data'=>$data,'id'=>$id, 'user_id'=>$user_id));
+        return $id;
+    }
 
-        public function delete_entry($entry_id, $user_id) {
-                $req = $this->_db->prepare("DELETE FROM data WHERE id=:entry_id AND user_id=:user_id ");
-                $req->execute(array('entry_id'=>$entry_id, 'user_id'=>$user_id));
-                return $id;
-        }
+    public function delete_entry($entry_id, $user_id) {
+        $req = $this->_db->prepare("DELETE FROM data WHERE id=:entry_id AND user_id=:user_id ");
+        $req->execute(array('entry_id'=>$entry_id, 'user_id'=>$user_id));
+        return $id;
+    }
 
-        public function check_entry($id, $user_id) {
-                $req = $this->_db->prepare("SELECT id FROM data WHERE id=:id AND user_id=:user_id ");
-                $req->execute(array('id'=>$id, 'user_id'=>$user_id));
-                $req = $req->fetchall(PDO::FETCH_ASSOC);
-                if (count($req) == 1)
-                        return true;
-                else
-                        return false;
-        }
-		
-		public function check_uuid($uuid, $user_id) {
-                $req = $this->_db->prepare("SELECT id FROM data WHERE category=:uuid AND user_id=:user_id ");
-                $req->execute(array('uuid'=>$uuid, 'user_id'=>$user_id));
-                $req = $req->fetchall(PDO::FETCH_ASSOC);
-                if (count($req) == 0)
-                        return true;
-                else
-                        return false;
-        }
+    public function check_entry($id, $user_id) {
+        $req = $this->_db->prepare("SELECT id FROM data WHERE id=:id AND user_id=:user_id ");
+        $req->execute(array('id'=>$id, 'user_id'=>$user_id));
+        $req = $req->fetchall(PDO::FETCH_ASSOC);
+        if (count($req) == 1)
+            return true;
+        else
+            return false;
+    }
+
+    public function check_uuid($uuid, $user_id) {
+        $req = $this->_db->prepare("SELECT id FROM data WHERE category=:uuid AND user_id=:user_id ");
+        $req->execute(array('uuid'=>$uuid, 'user_id'=>$user_id));
+        $req = $req->fetchall(PDO::FETCH_ASSOC);
+        if (count($req) == 0)
+            return true;
+        else
+            return false;
+    }
 
 }
--- script/install_db.php
+++ script/install_db.php
@@ -5,40 +5,40 @@ $db = new PDO(PDO_DNS);
 $driver = $db->getAttribute(PDO::ATTR_DRIVER_NAME);
 
 if ($driver == 'pgsql') {
-	
-	$req = "CREATE TABLE data (
-			id SERIAL PRIMARY KEY, 
-			data TEXT,
-			category VARCHAR(36),
-			user_id INT NOT NULL)";
-	$db->query($req);
-	
-	$req = "CREATE TABLE user (
-			id SERIAL PRIMARY KEY, 
-			username VARCHAR(256) NOT NULL UNIQUE,
-			verifier VARCHAR(256) NOT NULL,
-			salt VARCHAR(32) NOT NULL,
-			category TEXT NOT NULL,
-			config TEXT NOT NULL);";
-	$db->query($req);
+
+    $req = "CREATE TABLE data (
+            id SERIAL PRIMARY KEY, 
+            data TEXT,
+            category VARCHAR(36),
+            user_id INT NOT NULL)";
+    $db->query($req);
+
+    $req = "CREATE TABLE user (
+            id SERIAL PRIMARY KEY, 
+            username VARCHAR(256) NOT NULL UNIQUE,
+            verifier VARCHAR(256) NOT NULL,
+            salt VARCHAR(32) NOT NULL,
+            category TEXT NOT NULL,
+            config TEXT NOT NULL);";
+            $db->query($req);
 }
 else {
-	
-	$req = "CREATE TABLE data (
-			id INTEGER PRIMARY KEY, 
-			data TEXT,
-			category VARCHAR(36),
-			user_id INT NOT NULL)";
-	$db->query($req);
-	
-	$req = "CREATE TABLE user (
-			id INTEGER PRIMARY KEY,
-			username VARCHAR(256) NOT NULL UNIQUE,
-			verifier VARCHAR(256) NOT NULL,
-			salt VARCHAR(32) NOT NULL,
-			category TEXT NOT NULL,
-			config TEXT NOT NULL);";
-	$db->query($req);
-	
+
+    $req = "CREATE TABLE data (
+            id INTEGER PRIMARY KEY, 
+            data TEXT,
+            category VARCHAR(36),
+            user_id INT NOT NULL)";
+    $db->query($req);
+
+    $req = "CREATE TABLE user (
+            id INTEGER PRIMARY KEY,
+            username VARCHAR(256) NOT NULL UNIQUE,
+            verifier VARCHAR(256) NOT NULL,
+            salt VARCHAR(32) NOT NULL,
+            category TEXT NOT NULL,
+            config TEXT NOT NULL);";
+    $db->query($req);
+
 }
 
