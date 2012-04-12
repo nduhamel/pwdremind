2358ce9a70aa251ab228ec287e9f21e1ea4dc030
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-04-12 21:35:12 +0200

    Session class improvement and indentation

--- php/session.php
+++ php/session.php
@@ -1,70 +1,77 @@
 <?php
 class Session {
 
-	private $loginState;
+    private $_loginState;
 
-	public function __construct() {
-		session_start();
-	}
+    const CONNECTED = 1;
+    const NOT_CONNECTED = 0;
 
-	protected function login($username,$userid){
-		session_regenerate_id ();
-		$_SESSION['valid'] = True;
-		$_SESSION['username'] = $username;
-		$_SESSION['userid'] = $userid;
-		$_SESSION['LAST_ACTIVITY'] = time();
-		$this->loginState = True;
-	}
+    public function __construct() {
+        session_start();
+    }
 
-	public function is_logged_in(){
+    protected function login($username,$userid) {
+        session_regenerate_id ();
+        $_SESSION['valid'] = True;
+        $_SESSION['username'] = $username;
+        $_SESSION['userid'] = $userid;
+        $_SESSION['LAST_ACTIVITY'] = time();
+        $this->_loginState = Session::CONNECTED;
+    }
 
-		if (is_null($this->loginState)){
-			if(isset($_SESSION['valid']) && $_SESSION['valid'] && $this->check_activity() ){
-				$this->loginState = True;
-			}else{
-				$this->loginState = False;
-			}
-		}
+    public function is_logged_in() {
 
-		return $this->loginState;
-	}
+        if (is_null($this->_loginState)){
+            if(isset($_SESSION['valid']) && $_SESSION['valid'] && $this->check_activity() ){
+                $this->_loginState = Session::CONNECTED;
+            } 
+            else {
+                $this->_loginState = Session::NOT_CONNECTED;
+            }
+        }
 
-	public function logout(){
-		$_SESSION = array(); //destroy all of the session variables
-		session_destroy();
-		$this->loginState = False;
-	}
-	
-	private function check_activity(){
-		$time = time();
-		if( $time - $_SESSION['LAST_ACTIVITY']  <= SESSION_TIMEOUT ){
-			session_regenerate_id(true);
-			$_SESSION['LAST_ACTIVITY'] = $time;
-			return True;
-		}else{
-			$this->logout();
-			return False;
-		}
-	}
+        return $this->_loginState;
+    }
 
-	public function getValue($key, $default=NULL) {
-		if (isset($_SESSION[$key])){
-			return $_SESSION[$key];
-		} elseif ($default != NULL) {
-			throw new InvalidArgumentException($key.' not found');
-		} else {
-			return $default;
-		}
-	}
+    public function logout(){
+        $_SESSION = array(); //destroy all of the session variables
+        session_destroy();
+        $this->_loginState = Session::NOT_CONNECTED;
+    }
 
-	public function get_username() { return $_SESSION['username']; }
-	public function get_userid() { return $_SESSION['userid']; }
+    private function check_activity(){
+        $time = time();
+        if( $time - $_SESSION['LAST_ACTIVITY']  <= SESSION_TIMEOUT ){
+            session_regenerate_id(true);
+            $_SESSION['LAST_ACTIVITY'] = $time;
+            return True;
+        }
+        else {
+            $this->logout();
+            return False;
+        }
+    }
 
-	public function setValue($key, $value) {
-		$_SESSION[$key] = $value;
-	}
+    public function getValue($key, $default=NULL) {
+        if (isset($_SESSION[$key])){
+            return $_SESSION[$key];
+        } 
+        elseif ($default != NULL) {
+            throw new InvalidArgumentException($key.' not found');
+        } 
+        else {
+            return $default;
+        }
+    }
 
-	public function __destruct() {
-		session_write_close();
-	}
+    public function get_username() { return $_SESSION['username']; }
+    public function get_userid() { return $_SESSION['userid']; }
+
+    public function setValue($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    public function __destruct() {
+        session_write_close();
+    }
 }
