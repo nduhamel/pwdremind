ce475bc5eb5007203da1a99aca4ff10a700c27c4
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-06-25 14:11:06 +0200

    MySQL config help

--- config.php
+++ config.php
@@ -8,15 +8,19 @@
 //  sqlite:../test.db
 // postgresql exemple:
 //  pgsql:host=localhost;port=5432;dbname=pwdremind;user=pwdremind;password=pwdremind
-define('PDO_DSN', 'sqlite:../test.db');
+//mysql exemple:
+//	mysql:host=127.0.0.1;port=8889;dbname=pwdremind  
+// Use 127.0.0.1 instead of localhost to avoid using UNIX socket  
+define('PDO_DSN', 'mysql:host=127.0.0.1;port=8889;dbname=pwdremind');
 
 //Define your db username and password
-define('PDO_USER','username');
-define('PDO_PASSWORD','password');
+define('PDO_USER','root');
+define('PDO_PASSWORD','root');
 
+//Session timeout
 define("SESSION_TIMEOUT", 300);
 
 //Log
 ini_set("log_errors" , "1");
-ini_set("error_log" , "Errors.log.txt");
+ini_set("error_log" , "errors.log");
 ini_set("display_errors" , "0");
