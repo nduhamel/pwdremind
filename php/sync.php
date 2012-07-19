f9aae3c7630fbae09c11ce595daacaf1cd0498b3
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-07-19 18:28:27 +0200

    New sync

--- php/sync.php
+++ php/sync.php
@@ -3,15 +3,11 @@ require_once('database.php');
 require_once('srpsession.php');
 require_once('message.php');
 require_once('authentication.php');
+require_once('router.php');
 require_once('../config.php');
-
 
 class Sync
 {
-    private $_session;
-    private $_message;
-    private $_db;
-
     //Input datas
     private $_URI;                  //URI - /password/cat/id
     private $_method;               //GET POST DELETE
@@ -20,10 +16,6 @@ class Sync
 
     function __construct($inputs)
     {
-        $this->_session = new SrpSession();
-        $this->_message = new Message($this->_session->getKhex());
-        $this->_db = new Database();
-
         //HTTP inputs
         $this->_URI =       $this->_checkKey('URI', $inputs);
         $this->_rawInput =  $this->_checkKey('raw_input', $inputs);
@@ -36,7 +28,7 @@ class Sync
     }
 
     private function _checkAuth() {
-        if ( !$this->_session->isLogged() ) {
+        if ( !$session->isLogged() ) {
             header('HTTP/1.1 403 Forbidden');
             print 'Not logged';
             exit();
@@ -48,126 +40,95 @@ class Sync
         return array_key_exists($key, $array) ? $array[$key] : NULL;
     }
 
-    public function run(){
+    public function run() {
 
-        //Get all the datas from the URI
-        //EX /passwords/category/1
-        // 0 => passwords
-        // 1 => category
-        // 2 => 1
-        $URI = explode('/', $this->_URI);
-        array_shift($URI);
-        $URI[0] = '/'.$URI[0];
-        
-        switch ($URI[0]) {
+        $session = new SrpSession();
+        $message = new Message($session->getKhex());
+        $db = new Database();
+        $auth = new Authentication($db, $this->_username, $this->_A, $this->_M1);
 
-            /*
-                Authentication process 
-                /authentication
-            */    
-            case '/authentication':
-                $auth = new Authentication($this->_db,$this->_username,$this->_A,$this->_M1);
-                $auth->run();
-                break;
+        $URI = $this->_URI;
+        $rawInput = json_decode($this->_rawInput, true);
+        $method = $this->_method;
 
-            /*
-                GET all latest passwords
-                /passwords
-                
-                OR 
+        //Start the router
+        $router = new Router();
 
-                GET all passwords in a specific category
-                /passwords/category/:id
-            */
-            case '/passwords':
-                $this->_checkAuth();
-                if( isset($URI[1]) && isset($URI[2]) ) {
-                    //  /passwords/category/:id
-                    $id = $URI[2];
-                    $entries = $this->_db->getEntries($id, $this->_session->getUserid());
-                    $this->_message->setData($entries);
-                    $this->_message->send();
-                } else {
-                    //  /passwords
-                    $entries = $this->_db->lastestEntries($this->_session->getUserid());
-                    $this->_message->setData($entries);
-                    $this->_message->send();
-                }
-                break;
-             
-            /*
-                GET all categories
-                /categories
-            */
-            case '/categories':
-                $this->_checkAuth();
-                $categories = $this->_db->getCategories( $this->_session->getUserid() );
-                $this->_message->setData($categories);
-                $this->_message->send();
-                break;   
+        // GET homepage
+        $router->addRoute('GET', '/', function() {
+            header('Location : index.html');
+        });
 
-            // ADD | UPDATE | DELETE - password
-            case '/password':
-                $this->_checkAuth();
-                $json_a = json_decode($this->_rawInput, true);
-                switch ($this->_method) {
-                    //ADD
-                    case 'POST':
-                        $id = $this->_db->storeEntry($json_a['data'], $json_a['category_id'], $this->_session->getUserid());
-                        $this->_message->setData(array(
-                                        'id' => $id,
-                                        'data' => $json_a['data'],
-                                        'category_id' => $json_a['category_id'],
-                        ));
-                        break;
+        // Run authentication
+        $test = 'test';
+        $router->addRoute('POST', '/authentication', function() use ($auth) {
+            $auth->run();
+        });
 
-                    //UPDATE
-                    case 'PUT':
-                        $this->_db->updateEntry($json_a['id'], $json_a['data'], $json_a['category_id'], $this->_session->getUserid());
-                        $this->_message->setData(array(
-                                        'id' => $json_a['id'],
-                                        'data' => $json_a['data'],
-                                        'category_id' => $json_a['category_id'],
-                        ));
-                        break;
+        // GET all lastest passwords
+        $router->addRoute('GET', '/passwords', function() use ($db, $message, $session) {
+            $entries = $db->lastestEntries($session->getUserid());
+            $message->setData($entries);
+            $message->send();
+        });        
 
-                    //DELETE
-                    case 'DELETE':
-                        $this->_db->deleteEntry($json_a['id'], $this->_session->getUserid());
-                        $this->_message->setData(array(
-                                        'id' => $json_a['id'],
-                        ));
-                        break;
-                }
-                $this->_message->send();
-                break;
+        // GET passwords in a specific category
+        $router->addRoute('GET', '/passwords/category/:id', function($id) use ($db, $message, $session) {
+            $entries = $db->getEntries($id, $session->getUserid());
+            $message->setData($entries);
+            $message->send();
+        }); 
 
-            // ADD a new category
-            case '/category':
-                $this->_checkAuth();
-                $json_a = json_decode($this->_rawInput, true);
-                $id = $this->_db->addCategory($json_a['data'], $this->_session->getUserid());
-                $this->_message->setData(array(
-                                'id' => $id,
-                                'data' => $json_a['data'],
-                ));
-                $this->_message->send();
-                break;
+        //GET all categories
+        $router->addRoute('GET', '/categories', function() use ($db, $message, $session) {
+            $categories = $db->getCategories( $session->getUserid() );
+            $message->setData($categories);
+            $message->send();
+        });
 
+        // ADD a new password
+        $router->addRoute('POST', '/password', function() use ($db, $message, $session, $rawInput) {
+            $id = $db->storeEntry($rawInput['data'], $rawInput['category_id'], $session->getUserid());
+            $message->setData(array(
+                            'id' => $id,
+                            'data' => $rawInput['data'],
+                            'category_id' => $rawInput['category_id'],
+            ));
+            $message->send();
+        });
+  
+        // UPDATE a password
+        $router->addRoute('PUT', '/password', function() use ($db, $message, $session, $rawInput) {
+            $db->updateEntry($rawInput['id'], $rawInput['data'], $rawInput['category_id'], $session->getUserid());
+            $message->setData(array(
+                            'id' => $rawInput['id'],
+                            'data' => $rawInput['data'],
+                            'category_id' => $rawInput['category_id'],
+            ));
+            $message->send();
+        });
 
-            // Logout
-            case '/logout':
-                $this->_session->logout();
-                break;
+        // DELETE a password
+        $router->addRoute('DELETE', '/password', function() use ($db, $message, $session, $rawInput) {
+            $db->deleteEntry($rawInput['id'], $session->getUserid());
+            $message->setData(array(
+                            'id' => $rawInput['id'],
+            ));
+            $message->send();
+        });
 
-            // Redirect to main path
-            default:
-                //Nothing to do
-                break;
-        }
+        // ADD a new category
+        $router->addRoute('POST', '/category', function() use ($db, $message, $session, $rawInput) {
+            $id = $db->addCategory($rawInput['data'], $session->getUserid());
+            $message->setData(array(
+                            'id' => $id,
+                            'data' => $rawInput['data'],
+            ));
+            $message->send();
+        });
+
+        //Run the router
+        $router->run($this->_method, $this->_URI);
 
     }
-
 }
-
-
