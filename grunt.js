5aabd73e8f841b19d44cc7274e837a7639b2cd91
Nicolas Duhamel <nicolas@jombi.fr>
2012-08-13 03:35:01 +0200

    Add css compilation

--- grunt.js
+++ grunt.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib');
     grunt.loadNpmTasks('grunt-requirejs');
+    grunt.loadNpmTasks('grunt-recess');
 
     // ==========================================================================
     // TASKS
@@ -15,7 +16,6 @@ module.exports = function(grunt) {
         // this.flags === a map of flags specified after the target on the command-line
         // this.file === file-specific .src and .dest properties
 
-        // Log some stuff.
         var cheerio = require('cheerio');
         var contents = String(grunt.file.read(this.file.src, 'utf-8'));
         var $ = cheerio.load(contents);
@@ -30,7 +30,33 @@ module.exports = function(grunt) {
         // write out newly created file contents
         grunt.file.write(this.file.src, $.html(), 'utf-8');
 
-        grunt.log.writeln('Adapt '+this.file.src+' to almond successfully');
+        grunt.log.ok('Adapt '+this.file.src+' to almond');
+
+    });
+
+    grunt.registerMultiTask('cssToRecess', 'Add compiled css to html remove others', function() {
+        // this.target === the name of the target
+        // this.data === the target's value in the config object
+        // this.name === the task name
+        // this.args === an array of args specified after the target on the command-line
+        // this.flags === a map of flags specified after the target on the command-line
+        // this.file === file-specific .src and .dest properties
+
+        var cheerio = require('cheerio');
+        var contents = String(grunt.file.read(this.file.src, 'utf-8'));
+        var $ = cheerio.load(contents);
+
+        if (this.data.removeAll) {
+            $('link[rel="stylesheet"]').remove();
+        }
+
+        $('head').append('<link rel="stylesheet" href="'+this.data.cssPath+'">');
+
+
+        // write out newly created file contents
+        grunt.file.write(this.file.src, $.html(), 'utf-8');
+
+        grunt.log.ok('Adapt '+this.file.src+' to compiled css');
 
     });
 
@@ -46,6 +72,31 @@ module.exports = function(grunt) {
                 appPath: './js/app.js',
                 removeAll: true,
             },
+        },
+
+        cssToRecess : {
+            build: {
+                src: 'build/www/index.html',
+                cssPath: './css/main.css',
+                removeAll: true,
+            },
+        },
+
+
+        //CSS
+        recess: {
+            build: {
+                src: [
+                    "src/www/css/main.css",
+                    "src/www/css/bootstrap-responsive.css",
+                    "src/www/css/bootstrap.css",
+                ],
+                dest: 'build/www/css/main.css',
+                options: {
+                    compile: true,
+                    compress: true
+                }
+            }
         },
 
         clean: {
@@ -67,7 +118,6 @@ module.exports = function(grunt) {
               "build/": "src/config.php-dist",
               "build/script": "src/script/*",
               "build/www": "src/www/*",
-              "build/www/css": "src/www/css/**",
               "build/www/img": "src/www/img/**",
               "build/www/media": "src/www/media/**",
               "build/www/": "src/www/.htaccess",
@@ -83,7 +133,7 @@ module.exports = function(grunt) {
     });
 
     // build task
-    grunt.registerTask('build', 'clean:build copy:build requirejs requireToAlmond:build');
+    grunt.registerTask('build', 'clean:build copy:build requirejs recess:build cssToRecess:build requireToAlmond:build');
 
     // devbuild task
     grunt.registerTask('devbuild', 'build copy:dev');
