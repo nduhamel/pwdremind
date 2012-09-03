83f56097234f115f202d240a45f800c3dd7f30fb
Grégoire Delattre <gregoire.delattre@gmail.com>
2012-09-04 01:03:40 +0200

    Remove old file

--- script/db_connection_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('../config.php');
-
-	try {
-		$db = new PDO(PDO_DSN, PDO_USER, PDO_PASSWORD);
-		fwrite(STDOUT,"Connection succeeded! \n");
-	} catch (PDOException $e) {
-		fwrite(STDOUT,"Connection failed : ". $e->getMessage() ."\n");
-	}
